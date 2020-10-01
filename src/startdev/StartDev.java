c01e9eb4fb0a6fd85404e8ddcab4084d932bbf54
pupil <pupil@ivkhk.ee>
2020-10-01 08:36:28 +0300

    Завершена 7-ая задача

--- src/startdev/StartDev.java
+++ src/startdev/StartDev.java
@@ -26,14 +26,14 @@ public class StartDev {
 //        Task4 task4 = new Task4();
 //        task4.run();
 //        
-        Task5 task5 = new Task5();
-        task5.run();
+//        Task5 task5 = new Task5();
+//        task5.run();
+//        
+//        Task6 task6 = new Task6();
+//        task6.run();
         
-        Task6 task6 = new Task6();
-        task6.run();
-        
-//        Task7 task7 = new Task7();
-//        task7.run();
+        Task7 task7 = new Task7();
+        task7.run();
         }
     
 }
--- src/startdev/Task7.java
+++ src/startdev/Task7.java
@@ -13,18 +13,24 @@ import java.util.Scanner;
  */
 public class Task7 {
     public void run(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("----- Задача 7 ------");
         System.out.println("Введите строку");
-        Scanner scanner = new Scanner(System.in);
-        // посчитайте сколько раз в строке встречается введенная вами буква
-        int ent = 0;
-        System.out.println("Введите букву: ");
-        int entLetter = scanner.nextInt();
-        
-       
-        
-        System.out.println("Буква \""+letter+"\" встречается в тексте "+ n + "раз");
+        String str = scanner.nextLine();
+        System.out.println("Введите букву : ");
+        String letter = scanner.nextLine();
+       // посчитайте сколько раз в строке встречается введенная вами буква
+            
+        int count = 0;
+        int index = str.indexOf(letter);
+            while (index != -1){
+                count++;
+                str = str.substring(index + 1);
+                index = str.indexOf(letter);
+            }
+                       
+        System.out.println("Буква \""+letter+"\" встречается в тексте "+ count + "раз");
         System.out.println("");
-        System.out.println("----- конец задачи 6 ------");
+        System.out.println("----- конец задачи 7 ------");
     }
 }
